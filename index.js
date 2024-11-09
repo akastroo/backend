c04ea0bdfd437d45a6c3567d1bcff584dbac505e
akastroo <jvinicius2002@gmail.com>
2024-11-09 01:14:18 -0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Importando o módulo express para lidar com rotas e middlewares
 //import tarefaRoutes from "./routes/tarefaRoutes"
+require('dotenv').config();
 
-const cors = require('cors');
+const mongoose = require('mongoose');
+//const cors = require('cors');
 
 const express = require('express');
 
@@ -12,19 +14,36 @@ const app = express();
 //const tarefaRoutes = require('./routes/tarefaRoutes');
 
 // Definindo a porta em que o servidor irá escutar
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
+const employeeRoutes = require('./routes/employeeRoute');
 // Configurando as rotas da aplicação para utilizar as rotas 
 //relacionadas às tarefas
-app.use(cors());
+//app.use(cors());
 //app.use('/api', tarefaRoutes);
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo à API Backend');
 });
 
-// Inicializando o servidor e fazendo com que ele escute na porta definida
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
 
+
+// Configurando as rotas
+app.use('/api', employeeRoutes);
+
+// Inicializando o servidor e fazendo com que ele escute na porta definida
+/*app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});*/
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@viceri.wn7yt.mongodb.net/?retryWrites=true&w=majority&appName=viceri`,
+  )
+  .then(() => {
+    console.log('Conectou ao banco!');
+    // Agora, inicie o servidor Express após a conexão com o MongoDB
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err))
